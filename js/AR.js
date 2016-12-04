439b35103efd59a0200eb2b667830c695dec3eae
Robert Hucks <rahucks@gmail.com>
2016-12-04 21:31:36 +0000

    Added test video
    Added a test video in to the ThreeJS scene

--- js/AR.js
+++ js/AR.js
@@ -13,18 +13,35 @@ function startAR(pinID) {
     controls = new THREE.DeviceOrientationControls(camera);
     scene = new THREE.Scene();
     
+    //Wireframe box
     var geometry = new THREE.BoxGeometry( 100, 100, 100, 4, 4, 4 );
     var material = new THREE.MeshBasicMaterial( { color: 0xff00ff, side: THREE.BackSide, wireframe: true } );
     var mesh = new THREE.Mesh( geometry, material );
     scene.add( mesh );
 
-    renderer = new THREE.WebGLRenderer();
+    //Load Video
+    video = document.createElement('AR-video');
+    video.src = "vid/testvid.ogv";
+
+    videoTexture = new THREE.VideoTexture(video);
+    videoTexture.minFilter = THREE.LinearFilter;
+    videoTexture.magFilter = THREE.LinearFilter;
+
+    var movieMaterial = new THREE.MeshBasicMaterial( { map: videoTexture, overdraw: true, side: THREE.DoubleSide } );
+    var movieGeometry = new THREE.PlaneGeometry(240, 100, 4, 4);
+    var movieScreen = new THREE.Mesh(movieGeometry, movieMaterial);
+    movieScreen.position.set(0, 50, 0);
+    scene.add(movieScreen);
+
+
+    renderer = new THREE.WebGLRenderer( {alpha: true } );
     renderer.setPixelRatio( window.devicePixelRatio );
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.domElement.style.position = 'absolute';
     renderer.domElement.style.top = 0;
     container.appendChild(renderer.domElement);
 
+    //On window resize.. Probably wont need because it'll be fullscreen on phone anyway. Doesn't hurt.
     window.addEventListener('resize', function() {
 
         camera.aspect = window.innerWidth / window.innerHeight;
