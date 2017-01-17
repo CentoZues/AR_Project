f750ebce6444f1abc49ce0263812c55797cc2561
Robert Hucks <rahucks@gmail.com>
2017-01-17 21:53:30 +0000

    Added test background to scene.

--- js/AR.js
+++ js/AR.js
@@ -14,11 +14,21 @@ function startAR(pinID) {
     
     //Wireframe Sphere
     //var geometry = new THREE.BoxGeometry( 100, 100, 100, 4, 4, 4 );
-    var geometry = new THREE.SphereGeometry( 100, 20, 20 );
-    var material = new THREE.MeshBasicMaterial( { color: 0xff00ff, side: THREE.BackSide, wireframe: true } );
-    var mesh = new THREE.Mesh( geometry, material );
-    scene.add( mesh );
+    //var geometry = new THREE.SphereGeometry( 100, 20, 20 );
+    //var material = new THREE.MeshBasicMaterial( { color: 0xff00ff, side: THREE.BackSide, wireframe: true } );
+    //var mesh = new THREE.Mesh( geometry, material );
+    //scene.add( mesh );
 
+    //Background Image
+    var geometry = new THREE.SphereGeometry(500, 16, 8);
+    geometry.scale( - 1, 1, 1 );
+
+    var material = new THREE.MeshBasicMaterial( {
+        map: new THREE.TextureLoader().load('textures/backgroundTexture.jpg')
+    });
+    
+    var mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
 
     //Manage Video
     video = document.getElementById('video');
@@ -35,32 +45,32 @@ function startAR(pinID) {
 
 
 
-    //X Axis
-    var geometry = new THREE.BoxGeometry(5, 5, 5);
-    var material = new THREE.MeshBasicMaterial( { color: 0xFF0000 } );
-    var cube = new THREE.Mesh(geometry, material);
-    cube.position.x = 50; //Forward
-    cube.position.y = 0; //Up
-    cube.position.z = 0; //Side
-    scene.add(cube);
+    // //X Axis
+    // var geometry = new THREE.BoxGeometry(5, 5, 5);
+    // var material = new THREE.MeshBasicMaterial( { color: 0xFF0000 } );
+    // var cube = new THREE.Mesh(geometry, material);
+    // cube.position.x = 50; //Forward
+    // cube.position.y = 0; //Up
+    // cube.position.z = 0; //Side
+    // scene.add(cube);
 
-    //Y Axis
-    var geometry = new THREE.BoxGeometry(5, 5, 5);
-    var material = new THREE.MeshBasicMaterial( { color: 0x00FF00 } );
-    var cube = new THREE.Mesh(geometry, material);
-    cube.position.x = 0; //Forward
-    cube.position.y = 50; //Up
-    cube.position.z = 0; //Side
-    scene.add(cube);
+    // //Y Axis
+    // var geometry = new THREE.BoxGeometry(5, 5, 5);
+    // var material = new THREE.MeshBasicMaterial( { color: 0x00FF00 } );
+    // var cube = new THREE.Mesh(geometry, material);
+    // cube.position.x = 0; //Forward
+    // cube.position.y = 50; //Up
+    // cube.position.z = 0; //Side
+    // scene.add(cube);
 
-    //Z Axis
-    var geometry = new THREE.BoxGeometry(5, 5, 5);
-    var material = new THREE.MeshBasicMaterial( { color: 0x0000FF } );
-    var cube = new THREE.Mesh(geometry, material);
-    cube.position.x = 0; //Forward
-    cube.position.y = 0; //Up
-    cube.position.z = 50; //Side
-    scene.add(cube);
+    // //Z Axis
+    // var geometry = new THREE.BoxGeometry(5, 5, 5);
+    // var material = new THREE.MeshBasicMaterial( { color: 0x0000FF } );
+    // var cube = new THREE.Mesh(geometry, material);
+    // cube.position.x = 0; //Forward
+    // cube.position.y = 0; //Up
+    // cube.position.z = 50; //Side
+    // scene.add(cube);
 
     renderer = new THREE.WebGLRenderer( { alpha: true } );
     renderer.setPixelRatio( window.devicePixelRatio );
