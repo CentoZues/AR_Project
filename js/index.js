38b0f3924bb140e4145c42bb417d4f268a70342f
CentoZues <Devonvanderberg@hotmail.co.uk>
2017-06-28 13:53:23 +0100

    html test

--- js/index.js
+++ js/index.js
@@ -364,24 +364,30 @@ function pageModal(closestPin){
 	modalid = walkManager.getWalk(0).getPin(closestPin).getID();
 	var modalName = walkManager.getWalk(0).getPin(closestPin).getName();
 		modalHTML += '<div class="ui modal">\
-		  <i class="close icon"></i>\
-		  <div class="header">\
-		    You Have Arrived at '+ modalName +'\
-		  </div>\
-		    <div class="description">\
-		      <div class="ui header"> Would you like to Access the content for this '+ modalName +'?</div>\
-		    </div>\
-		  </div>\
-		  <div class="actions">\
-		    <div class="ui black deny button">\
-		      No Thank You!\
-		    </div>\
-		    <div id="modalButton" class="ui positive right labeled icon button">\
-		      Yes, Please!\
-		      <i class="checkmark icon"></i>\
-		    </div>\
-		  </div>\
-		</div>';
+  <i class="close icon"></i>\
+  <div class="header">\
+    Profile Picture\
+  </div>\
+  <div class="image content">\
+    <div class="ui medium image">\
+      <img src="/images/avatar/large/chris.jpg">\
+    </div>\
+    <div class="description">\
+      <div class="ui header"> Weve auto-chosen a profile image for you.</div>\
+      <p>Weve grabbed the following image from the \
+      <p>Is it okay to use this photo?</p>\
+    </div>\
+  </div>\
+  <div class="actions">\
+    <div class="ui black deny button">\
+      Nope\
+    </div>\
+    <div class="ui positive right labeled icon button">\
+      Yep, thats me\
+      <i class="checkmark icon"></i>\
+    </div>\
+  </div>\
+</div>';
 		$('#PinModal').append(modalHTML);
 		modalActive = 1;
 		console.log("html created");
