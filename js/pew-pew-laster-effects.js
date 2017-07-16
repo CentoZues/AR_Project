765245e3419fabfe1f1a6f52ecc7fc1d9088f23b
Renee <renee@beseen-marketing.co.uk>
2017-07-16 16:02:56 +0100

    Dialog tab

--- js/pew-pew-laster-effects.js
+++ js/pew-pew-laster-effects.js
@@ -37,4 +37,18 @@ $(function() {
 	}());  
 		
 	}
+	
+	
+	$( "#walkContent" ).on( "click", ".dialog", function() {
+	  	$('.notes').hide();
+		$('.dial').show();
+		$('.dialog').html('<i class="fa fa-file-text-o" aria-hidden="true"></i> Notes').addClass('note').removeClass('dialog');
+	});
+	$( "#walkContent" ).on( "click", ".note", function() {
+	  	$('.dial').hide();
+		$('.notes').show();
+		$('.dialog').html('<i class="fa fa-commenting-o" aria-hidden="true"></i> Dialog').addClass('dialog').removeClass('note');
+	});
+
+
 });
