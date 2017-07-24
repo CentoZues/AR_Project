b0894235bc7e558fbcec59f900680ca7f33fe9f6
Renee <renee@beseen-marketing.co.uk>
2017-07-24 21:34:03 +0100

    dialog fix

--- js/pew-pew-laster-effects.js
+++ js/pew-pew-laster-effects.js
@@ -47,7 +47,7 @@ $(function() {
 	}
 	
 	
-	$( "#walkContent" ).on( "click touchstart", ".dialog", function() {
+	$( "#walkContent" ).on( "click", ".dialog", function() {
 		if($(this).hasClass('note')){
 			$('.dial').hide();
 			$('.notes').show();
